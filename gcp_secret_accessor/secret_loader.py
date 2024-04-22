8b477a9398711a7d018da1deaaa728499e610fee
Nirvan <nirvan.sharma@myguardiangroup.com>
2024-04-22 16:16:55 -0400

    restructuring functions for simplicity

--- gcp_secret_accessor/secret_loader.py
+++ gcp_secret_accessor/secret_loader.py
@@ -2,23 +2,14 @@ from google.cloud import secretmanager
 import json
 
 class SecretLoader:
-    def __init__(self, project_id, secret_id, version_id):
-        self.project_id = project_id
-        self.secret_id = secret_id
-        self.version_id = version_id
-        if project_id is None or secret_id is None or version_id is None:
-            print("Some dependencies are missing.")
-        else:
-            print("You can go ahead and call the load_creds function.")
 
-
-    def load_creds(self):
+    def load_secret(self,project_id, secret_id, version_id):
         # Create the Secret Manager client.
         client = secretmanager.SecretManagerServiceClient()
         # Build the resource name of the secret version.
-        name = f"projects/{self.project_id}/secrets/{self.secret_id}/versions/{self.version_id}"
+        name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
         # Access the secret version.
         response = client.access_secret_version(name=name)
         # Return the decoded payload.
-        snowflake_creds = json.loads(response.payload.data.decode('UTF-8'))
-        return snowflake_creds
+        secret = json.loads(response.payload.data.decode('UTF-8'))
+        return secret
