aad80c27c7123c28f90dd3b4f1eba626c61dc888
Nirvan <nirvan.sharma@myguardiangroup.com>
2024-04-22 16:20:39 -0400

    added utility param to load secret function

--- gcp_secret_accessor/secret_loader.py
+++ gcp_secret_accessor/secret_loader.py
@@ -2,14 +2,23 @@ from google.cloud import secretmanager
 import json
 
 class SecretLoader:
+    def __init__(self, project_id):
+        self.project_id = project_id
+        if project_id is None or secret_id is None or version_id is None:
+            print("Some dependencies are missing.")
+        else:
+            print("You can go ahead and call the load_creds function.")
 
-    def load_secret(self,project_id, secret_id, version_id):
+
+    def load_secret(self,secret_id, version_id, json_payload=True):
         # Create the Secret Manager client.
         client = secretmanager.SecretManagerServiceClient()
         # Build the resource name of the secret version.
-        name = f"projects/{project_id}/secrets/{secret_id}/versions/{version_id}"
+        name = f"projects/{self.project_id}/secrets/{secret_id}/versions/{version_id}"
         # Access the secret version.
         response = client.access_secret_version(name=name)
         # Return the decoded payload.
-        secret = json.loads(response.payload.data.decode('UTF-8'))
-        return secret
+        if json_payload:
+            return json.loads(response.payload.data.decode('UTF-8'))
+        else:
+            return response.payload.data.decode('UTF-8')
